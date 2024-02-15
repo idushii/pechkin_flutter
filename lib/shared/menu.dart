7e45c9b957dbfe4afe18fa9fcc8a3489ff73aa47
Nicolay I <nic.ilinov@gmail.com>
2024-02-15 15:23:53 +0700

    fix const

--- lib/shared/menu.dart
+++ lib/shared/menu.dart
@@ -11,72 +11,72 @@ class Menu extends StatelessWidget {
     final isSmallScreen = MediaQuery.of(context).size.width < 700;
 
     if (isSmallScreen) {
-      return Container(
+      return SizedBox(
           width: 50,
           child: Column(children: [
-            Container(
+            const SizedBox(
               height: 50,
-              child: const Center(child: Icon(Icons.menu)),
+              child: Center(child: Icon(Icons.menu)),
             ),
             IconButton(
-              icon: Icon(Icons.folder_open),
+              icon: const Icon(Icons.folder_open),
               onPressed: () => context.goNamed(ProjectsScreen.route),
             ),
             IconButton(
-              icon: Icon(Icons.notifications),
+              icon: const Icon(Icons.notifications),
               onPressed: () => context.goNamed(ProjectsScreen.route),
             ),
             IconButton(
-              icon: Icon(Icons.chat_bubble_outline),
+              icon: const Icon(Icons.chat_bubble_outline),
               onPressed: () => context.goNamed(ProjectsScreen.route),
             ),
             IconButton(
-              icon: Icon(Icons.people),
+              icon: const Icon(Icons.people),
               onPressed: () => context.goNamed(ProjectsScreen.route),
             ),
           ]));
     }
 
-    return Container(
+    return SizedBox(
       width: 270,
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Container(height: 50, alignment: Alignment.center, child: Text("Меню", style: TextStyle(fontSize: 20))),
+        Container(height: 50, alignment: Alignment.center, child: const Text("Меню", style: TextStyle(fontSize: 20))),
         ListTile(
-            title: Text("Проекты"),
-            trailing: Icon(Icons.chevron_right),
-            leading: Icon(Icons.folder_open),
+            title: const Text("Проекты"),
+            trailing: const Icon(Icons.chevron_right),
+            leading: const Icon(Icons.folder_open),
             onTap: () {
               context.goNamed(ProjectsScreen.route);
             }),
         ListTile(
             title: Row(children: [
-              Text("Уведомления"),
+              const Text("Уведомления"),
               Container(
-                  decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                  decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.red),
                   alignment: Alignment.center,
                   height: 20,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5),
                     child: Text("10", style: TextStyle(color: Colors.white, fontSize: 10)),
                   ),
-                  margin: EdgeInsets.only(left: 10))
+                  margin: const EdgeInsets.only(left: 10))
             ]),
-            trailing: Icon(Icons.chevron_right),
-            leading: Icon(Icons.notifications),
+            trailing: const Icon(Icons.chevron_right),
+            leading: const Icon(Icons.notifications),
             onTap: () {
               context.goNamed(ProjectsScreen.route);
             }),
         ListTile(
-            title: Text("Обсуждения"),
-            trailing: Icon(Icons.chevron_right),
-            leading: Icon(Icons.chat_bubble_outline),
+            title: const Text("Обсуждения"),
+            trailing: const Icon(Icons.chevron_right),
+            leading: const Icon(Icons.chat_bubble_outline),
             onTap: () {
               context.goNamed(ProjectsScreen.route);
             }),
         ListTile(
-            title: Text("Команда"),
-            leading: Icon(Icons.people),
-            trailing: Icon(Icons.chevron_right),
+            title: const Text("Команда"),
+            leading: const Icon(Icons.people),
+            trailing: const Icon(Icons.chevron_right),
             onTap: () {
               context.goNamed(ProjectsScreen.route);
             }),
