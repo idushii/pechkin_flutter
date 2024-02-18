0d70820950d8319b4c3c51292238de253f30027d
Nicolay I <nic.ilinov@gmail.com>
2024-02-18 23:25:11 +0700

    Работа над отображением нагрузки, версия для телефона

--- lib/screens/project/project_request_screen.dart
+++ lib/screens/project/project_request_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pechkin_flutter/models/project_item.dart';
+import 'package:pechkin_flutter/models/project_request.dart';
 import 'package:pechkin_flutter/screens/project/widgets/project_request/project_view_request.dart';
 import 'package:pechkin_flutter/state/mocks.dart';
 
@@ -14,14 +16,20 @@ class ProjectRequestScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final project = mockProjects.firstWhere((project) => project.id == projectId, orElse: () => ProjectItem.empty());
+    final request = mockProjectRequests.firstWhere((element) => element.id == requestId, orElse: () => ProjectRequest.empty());
+    if (request.id == 0) {
+      return const Center(child: Text('Запрос не найден'));
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Проект ${projectId} Запрос ${requestId}"),
+        title: const Text("Просмотр запроса"),
       ),
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.only(left: 10),
-          child: ProjectViewRequest(id: requestId),
+          padding: const EdgeInsets.only(left: 10),
+          child: SingleChildScrollView(child: ProjectViewRequest(id: requestId)),
         ),
       ),
     );
