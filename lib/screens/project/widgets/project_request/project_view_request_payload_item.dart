dc2d5c9bd82b244a9f3bc9008574da8f04ead62f
Nicolay I <nic.ilinov@gmail.com>
2024-03-08 20:49:36 +0700

    Форма редактирования ответа и нагрузки

--- lib/screens/project/widgets/project_request/project_view_request_payload_item.dart
+++ lib/screens/project/widgets/project_request/project_view_request_payload_item.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pechkin_flutter/models/project_request.dart';
@@ -13,38 +15,67 @@ class ProjectViewRequestPayloadItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final name = "${payload.name}${(payload.type == ProjectRequestPayloadType.ARRAY || payload.isArray) ? '[]' : ''}";
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        if (payload.path.isNotEmpty)
-          Container(
-            decoration: const BoxDecoration(border: Border(left: BorderSide(color: Colors.grey))),
-            width: 10,
-            height: isLast ? 10 : 20,
-            margin: EdgeInsets.only(bottom: isLast ? 10 : 0),
-          ),
-        for (var i = 0; i < payload.path.length; i++) const SizedBox(width: 20, height: 20),
-        Column(
-          children: [
+    final textFieldStyle = InputDecoration(
+      contentPadding: MediaQuery.of(context).size.width > 600
+          ? const EdgeInsets.symmetric(horizontal: 10, vertical: 5)
+          : const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+      isDense: true,
+      border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey), borderRadius: BorderRadius.zero),
+    );
+
+    final double size = isEdit ? 20 : 10;
+
+    return Form(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (payload.path.isNotEmpty)
             Container(
-              decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey), left: BorderSide(color: Colors.grey))),
-              width: 10,
-              height: 10,
+              decoration: const BoxDecoration(border: Border(left: BorderSide(color: Colors.grey))),
+              width: size,
+              height: isLast ? size : size * 2,
+              margin: EdgeInsets.only(bottom: isLast ? size : 0),
             ),
+          for (var i = 0; i < payload.path.length; i++) SizedBox(width: size * 2, height: size * 2),
+          Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey), left: BorderSide(color: Colors.grey))),
+                width: size,
+                height: size,
+              ),
+              Container(
+                decoration: isLast ? null : const BoxDecoration(border: Border(left: BorderSide(color: Colors.grey))),
+                width: size,
+                height: size,
+              )
+            ],
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            flex: 2,
+            child: isEdit ? TextFormField(controller: TextEditingController(text: name), decoration: textFieldStyle) : Text(name),
+          ),
+          if (payload.type != ProjectRequestPayloadType.ARRAY && payload.type != ProjectRequestPayloadType.OBJECT) ...[
             Container(
-              decoration: isLast ? null : const BoxDecoration(border: Border(left: BorderSide(color: Colors.grey))),
-              width: 10,
-              height: 10,
-            )
+              width: 130,
+                child: isEdit
+                    ? DropdownButtonFormField<String>(
+                        value: payload.type,
+                        items: [for (var v in ProjectRequestPayloadType.values) DropdownMenuItem(value: v, child: Text(v))],
+                        decoration: textFieldStyle,
+                        onChanged: (value) {
+                          // TODO
+                        })
+                    : Text(payload.type)),
+            Expanded(
+                flex: 3,
+                child: isEdit
+                    ? TextFormField(controller: TextEditingController(text: payload.description), decoration: textFieldStyle)
+                    : Text(payload.description, maxLines: 1, overflow: TextOverflow.ellipsis)),
           ],
-        ),
-        const SizedBox(width: 10),
-        Expanded(child: Text(name)),
-        if (payload.type != ProjectRequestPayloadType.ARRAY && payload.type != ProjectRequestPayloadType.OBJECT) ...[
-          Expanded(child: Text(payload.description, maxLines: 1, overflow: TextOverflow.ellipsis)),
-          Expanded(child: Text(payload.type)),
         ],
-      ],
+      ),
     );
   }
 }
